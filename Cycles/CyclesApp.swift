7a13ed1820711c22f227b089f3f3ec67ab9a85d3
slpuppy <puppizin@gmail.com>
2024-05-06 15:16:23 -0300

    Initial Commit

---
+++ Cycles/CyclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyclesApp.swift
+//  Cycles
+//
+//  Created by Gabriel Puppi on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CyclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
